dadf4bc941580db0c89ceec1d9231e5fc7828d7a
radhikapathak04 <radhikapathak7974@gmail.com>
2026-02-18 10:51:33 +0530

    update sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,58 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://panditjiujjain.vercel.app",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://panditjiujjain.vercel.app/vivah-puja",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://panditjiujjain.vercel.app/griha-pravesh",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://panditjiujjain.vercel.app/mangal-dosh-puja",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://panditjiujjain.vercel.app/bhat-puja",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://panditjiujjain.vercel.app/dosh-nivaran",
+      lastModified: new Date(),
+    },
+
+    
+{
+      url: "https://panditjiujjain.vercel.app/baglamukhi-havan",
+      lastModified: new Date(),
+    },{
+      url: "https://panditjiujjain.vercel.app/vyapar-vahan-puja",
+      lastModified: new Date(),
+    },{
+      url: "https://panditjiujjain.vercel.app/tyohar-puja",
+      lastModified: new Date(),
+    },{
+      url: "https://panditjiujjain.vercel.app/santaan-puja",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://panditjiujjain.vercel.app/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://panditjiujjain.vercel.app/kundali-vishleshan",
+      lastModified: new Date(),
+    },
+  ];
+}
