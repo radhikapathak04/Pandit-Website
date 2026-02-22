4663c1529e17f2e59abe83fbc675c5134b4f3f0e
radhikapathak04 <radhikapathak7974@gmail.com>
2026-02-22 19:20:46 +0530

    update url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,55 +3,55 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://panditjiujjain.vercel.app",
+      url: "https://gurujiujjain.com",
       lastModified: new Date(),
     },
 
     {
-      url: "https://panditjiujjain.vercel.app/vivah-puja",
+      url: "https://gurujiujjain.com/vivah-puja",
       lastModified: new Date(),
     },
 
     {
-      url: "https://panditjiujjain.vercel.app/griha-pravesh",
+      url: "https://gurujiujjain.com/griha-pravesh",
       lastModified: new Date(),
     },
 
     {
-      url: "https://panditjiujjain.vercel.app/mangal-dosh-puja",
+      url: "https://gurujiujjain.com/mangal-dosh-puja",
       lastModified: new Date(),
     },
 
     {
-      url: "https://panditjiujjain.vercel.app/bhat-puja",
+      url: "https://gurujiujjain.com/bhat-puja",
       lastModified: new Date(),
     },
 
     {
-      url: "https://panditjiujjain.vercel.app/dosh-nivaran",
+      url: "https://gurujiujjain.com/dosh-nivaran",
       lastModified: new Date(),
     },
 
     
 {
-      url: "https://panditjiujjain.vercel.app/baglamukhi-havan",
+      url: "https://gurujiujjain.com/baglamukhi-havan",
       lastModified: new Date(),
     },{
-      url: "https://panditjiujjain.vercel.app/vyapar-vahan-puja",
+      url: "https://gurujiujjain.com/vyapar-vahan-puja",
       lastModified: new Date(),
     },{
-      url: "https://panditjiujjain.vercel.app/tyohar-puja",
+      url: "https://gurujiujjain.com/tyohar-puja",
       lastModified: new Date(),
     },{
-      url: "https://panditjiujjain.vercel.app/santaan-puja",
+      url: "https://gurujiujjain.com/santaan-puja",
       lastModified: new Date(),
     },
     {
-      url: "https://panditjiujjain.vercel.app/contact",
+      url: "https://gurujiujjain.com/contact",
       lastModified: new Date(),
     },
     {
-      url: "https://panditjiujjain.vercel.app/kundali-vishleshan",
+      url: "https://gurujiujjain.com/kundali-vishleshan",
       lastModified: new Date(),
     },
   ];
